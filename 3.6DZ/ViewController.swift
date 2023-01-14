7bf14f303e9223cb16e609f0ecaa12bf93c64f0e
bembi <ismanovvvabembi123>
2023-01-14 12:02:53 +0600

    Initial Commit

---
+++ 3.6DZ/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  3.6DZ
+//
+//  Created by 지구9 on 14/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
